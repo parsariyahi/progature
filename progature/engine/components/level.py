34d213b4373d860dacbf6126f425b664d228e89d
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-08 13:04:59 +0330

    refactore(components): Add ``quests`` to ``Level``

--- progature/engine/components/level.py
+++ progature/engine/components/level.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+from typing import List
+
+from progature.engine.components.quest import Quest
+
 class Level:
     
-    def __init__(self, name):
+    def __init__(self, name: str, quests: List[Quest] = None) -> str:
         self.name = name
+        self.quests = quests

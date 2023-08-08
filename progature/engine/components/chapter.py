e9b576fa84e0e0fb5e5bedbbcda930428746458f
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-08 13:05:34 +0330

    refactore(components): Add ``levels`` to ``Chapter``

--- progature/engine/components/chapter.py
+++ progature/engine/components/chapter.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+from typing import List
+
+from progature.engine.components.level import Level
+
 class Chapter:
 
-    def __init__(self, name):
+    def __init__(self, name: str, levels: List[Level]):
         self.name = name
+        self.levels = levels

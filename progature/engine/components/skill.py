4a24b735c3fb5720a12b2e0eacb6287d5fa638f7
parsariyahi <pany.parsariyahi@gmail.com>
2023-08-08 15:49:42 +0330

    refactore(component): Add ``chapters`` to ``skill``

--- progature/engine/components/skill.py
+++ progature/engine/components/skill.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+from typing import List 
+
+from progature.engine.components.chapter import Chapter
+
 class Skill:
 
-    def __init__(self, name):
+    def __init__(self, name, chapters: List[Chapter] = None):
         self.name = name
+        self.chapters = chapters

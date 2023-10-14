3e3221a1f1e880f6eaa29644495b13f8a9268887
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-14 10:42:04 +0330

    refactor(games): changed ``tests`` for ``games`` module

--- tests/games/test_game_loader.py
+++ tests/games/test_game_loader.py
@@ -2,11 +2,11 @@ import sys
 import os
 from pathlib import Path
 
-from progature.games import GameLoader
+from progature.games.loader import GameLoader
 
 def test_game_loader():
     game_name = "py_game.json"
-    py_game_path = Path("progature/games") / game_name
+    py_game_path = Path("progature/games/files") / game_name
     game = GameLoader.load(py_game_path.absolute())
 
     # assert game["skill"] == "basic python"

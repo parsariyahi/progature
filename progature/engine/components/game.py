7fa110efccfd458546e47f094483b668d7afacf8
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-16 11:14:58 +0330

    refactor(components): moved ``chapters`` from ``skill`` component to ``game`` compoent

--- progature/engine/components/game.py
+++ progature/engine/components/game.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
-from typing import Optional, Union
+from typing import Optional, Union, List
 
 from progature.engine.components.skill import Skill
+from progature.engine.component.chapter import Chapter
 
 class Game:
 
-    def __init__(self, name, skill: Optional[Skill | None] = None, is_complete=False) -> None:
+    def __init__(self, name, skill: Optional[Skill | None] = None, chapters: Optional[List[Chapter] | None] = None, is_complete=False) -> None:
         self.name = name
         self.skill = skill
+        self.chapters = chapters
         self.is_complete = is_complete
     
     @property
@@ -17,3 +19,11 @@ class Game:
     @skill.setter
     def skill(self, skill: Optional[Skill | None]):
         self._skill = skill
+
+    @property
+    def chapters(self) -> Union[Skill, None]:
+        return self._chapters
+
+    @chapters.setter
+    def chapters(self, chapters: Optional[List[Chapter] | None]):
+        self._chapters = chapters
--- progature/engine/components/skill.py
+++ progature/engine/components/skill.py
@@ -1,18 +1,4 @@
-from __future__ import annotations
-from typing import List, Optional, Union
-
-from progature.engine.components.chapter import Chapter
-
 class Skill:
 
-    def __init__(self, name, chapters: Optional[List[Chapter] | None] = None) -> None:
+    def __init__(self, name) -> None:
         self.name = name
-        self.chapters = chapters
-
-    @property
-    def chapters(self) -> Union[List[Chapter], None]:
-        return self._chapters
-
-    @chapters.setter
-    def chapters(self, chapters: Optional[List[Chapter] | None]) -> None:
-        self._chapters = chapters

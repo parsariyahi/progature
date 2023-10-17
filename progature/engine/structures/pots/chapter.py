08103ba427c69f8e429ba5f1483fd862ce6aa545
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-17 10:56:59 +0330

    bug(structures): fixed ``circular import`` error for each ``Pot``

--- progature/engine/structures/pots/chapter.py
+++ progature/engine/structures/pots/chapter.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Iterable
 
 from progature.engine.structures.pots.pot import Pot
-from progature.engine.components import Chapter
+# from progature.engine.components import Chapter
 
 class ChapterPot(Pot):
     pass
--- progature/engine/structures/pots/quest.py
+++ progature/engine/structures/pots/quest.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Iterable
 
 from progature.engine.structures.pots.pot import Pot
-from progature.engine.components import Quest
+# from progature.engine.components import Quest
 
 class QuestPot(Pot):
     pass

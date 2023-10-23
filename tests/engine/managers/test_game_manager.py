9082ea552476f6c7b0d25fb1138e61dfc800cd78
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-23 10:53:22 +0330

    tests(managers): add tests for ``GameManger``

---
+++ tests/engine/managers/test_game_manager.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from progature.engine.core.managers import GameManager
+from progature.engine.core.game.loader import GameLoader
+
+
+def test_game_manager_init():
+    game_name = "py_game.json"
+    py_game_path = Path("progature/games") / game_name
+    game = GameLoader.load(py_game_path.absolute())
+    manager = GameManager(game)
+
+    assert manager.game_name() == game.name
+    assert manager.game_skill()  == game.skill
+    assert manager.current_chapter() == game.chapters[0]
+    assert manager.next_cahpter() == game.chapters[1]
+    assert manager.nth_chapter(2) == game.chapters[2]
+    
+    

df6d488577e47284b13dd3ae8754b9f7ad464201
parsariyahi <pany.parsariyahi@gmail.com>
2023-10-23 10:51:39 +0330

    feature(managers): add ``GameManager``

---
+++ progature/engine/core/managers/game.py
@@ -0,0 +1,35 @@
+from typing import Union
+
+from progature.engine.components import (
+    Game, Skill
+)
+from progature.engine.structures.pots import (
+    ChapterPot,
+    LevelPot,
+    QuestPot,
+)
+
+class GameManager:
+
+    def __init__(self, game: Game):
+        self.game = game
+        self.current_chapter_index = 0
+
+    def game_name(self) -> str:
+        return self.game.name
+
+    def game_skill(self) -> Union[Skill, None]:
+        return self.game.skill
+
+    def chapters(self) -> Union[ChapterPot, None]:
+        return self.game.chapters
+
+    def current_chapter(self) :
+        return self.game.chapters[self.current_chapter_index]
+
+    def next_cahpter(self):
+        self.current_chapter_index += 1
+        return self.current_chapter()
+
+    def nth_chapter(self, index):
+        return self.game.chapters[index]

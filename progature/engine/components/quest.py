1d23b985c7f706eb03e11d75c6987241953d1ac8
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-05 10:13:54 +0330

    feature(components): add ``index`` to ``quest`` component

--- progature/engine/components/quest.py
+++ progature/engine/components/quest.py
@@ -1,6 +1,7 @@
 class Quest:
     
-    def __init__(self, name, is_complete=False):
+    def __init__(self, index: int, name, is_complete=False):
+        self.index = index
         self.name = name
         self.is_complete = is_complete
 

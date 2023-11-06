ca4ce7d5e458784b9d36044c500894e08620c49c
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-06 10:27:31 +0330

    tests(handlers): add tests for ``complete`` methods in ``GameHandler``

--- tests/engine/core/game/test_game_handler.py
+++ tests/engine/core/game/test_game_handler.py
@@ -60,3 +60,47 @@ def test_game_handler_write_on_quest():
     with GameHandler(game) as handler:
         handler._write_on_quest("name", "create int obj", 0, 0, 0)
         handler._write_on_quest("is_complete", True, 1, 1, 1)
+
+def test_game_handler_game_complete():
+    game_name = "test_game.json"
+    py_game_path = Path("progature/games") / game_name
+
+    with open(py_game_path.absolute(), "r") as file:
+        game_json = json.load(file)
+
+    game = GameLoader.load(py_game_path)
+    with GameHandler(game) as handler:
+        handler.game_complete()
+
+def test_game_handler_chapter_complete():
+    game_name = "test_game.json"
+    py_game_path = Path("progature/games") / game_name
+
+    with open(py_game_path.absolute(), "r") as file:
+        game_json = json.load(file)
+
+    game = GameLoader.load(py_game_path)
+    with GameHandler(game) as handler:
+        handler.chapter_complete(1)
+
+def test_game_handler_level_complete():
+    game_name = "test_game.json"
+    py_game_path = Path("progature/games") / game_name
+
+    with open(py_game_path.absolute(), "r") as file:
+        game_json = json.load(file)
+
+    game = GameLoader.load(py_game_path)
+    with GameHandler(game) as handler:
+        handler.level_complete(1, 0)
+
+def test_game_handler_quest_complete():
+    game_name = "test_game.json"
+    py_game_path = Path("progature/games") / game_name
+
+    with open(py_game_path.absolute(), "r") as file:
+        game_json = json.load(file)
+
+    game = GameLoader.load(py_game_path)
+    with GameHandler(game) as handler:
+        handler.quest_complete(1, 0, 0)

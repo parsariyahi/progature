879026d69d720e68c967f5d4e93c8622cba6565a
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-06 15:53:58 +0330

    tests(handlers): remove ``GameLoader`` from ``GameHandler`` tests

--- tests/engine/core/game/test_game_handler.py
+++ tests/engine/core/game/test_game_handler.py
@@ -7,33 +7,20 @@ from progature.engine.core.game.loader import GameLoader
 def test_game_handler():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
-
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    handler = GameHandler(game)
+    handler = GameHandler(py_game_path.absolute())
 
 def test_game_handler_write_on_game():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_game("skill", "basic python3")
 
 def test_game_handler_write_on_chapter():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_chapter("name", "vars", 0)
         handler._write_on_chapter("name", "loooops", 1)
 
@@ -41,11 +28,7 @@ def test_game_handler_write_on_level():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_level("name", "ints", 0, 0)
         handler._write_on_level("name", "for loooops", 1, 1)
 
@@ -53,11 +36,7 @@ def test_game_handler_write_on_quest():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_quest("name", "create int obj", 0, 0, 0)
         handler._write_on_quest("is_complete", True, 1, 1, 1)
 
@@ -65,42 +44,26 @@ def test_game_handler_game_complete():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler.game_complete()
 
 def test_game_handler_chapter_complete():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler.chapter_complete(1)
 
 def test_game_handler_level_complete():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler.level_complete(1, 0)
 
 def test_game_handler_quest_complete():
     game_name = "test_game.json"
     py_game_path = Path("progature/games") / game_name
 
-    with open(py_game_path.absolute(), "r") as file:
-        game_json = json.load(file)
-
-    game = GameLoader.load(py_game_path)
-    with GameHandler(game) as handler:
+    with GameHandler(py_game_path.absolute()) as handler:
         handler.quest_complete(1, 0, 0)

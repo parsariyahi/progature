47e790b8ecf8692ec15267f60cb8f406692c8bbd
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-07 10:45:58 +0330

    recator(tests): change ``pots`` to ``Pot`` ``Generic`` class

--- tests/engine/managers/test_quest_manager.py
+++ tests/engine/managers/test_quest_manager.py
@@ -1,13 +1,13 @@
 from tests.utils.component import QuestUtil
 
-from progature.engine.structures.pots import QuestPot
+from progature.engine.structures import Pot
 from progature.engine.components import Quest
 from progature.engine.core.managers import QuestManager
 
 
 def test_quest_manager_init():
     quests = QuestUtil.create_quest_bulk()
-    pot = QuestPot(quests)
+    pot = Pot(quests)
     manager = QuestManager(pot)
 
     assert manager.current_quest() == pot[0]

1f203ce4cfd48c67eed1a1af174df91fb82af63c
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-09 10:08:29 +0330

    feature(main): add ``progature`` to ``sys.path``

--- progature/main.py
+++ progature/main.py
@@ -1,10 +1,14 @@
+import os
+import sys
 from pathlib import Path
 import PySimpleGUI as pg
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from progature.engine.core.game.loader import GameLoader
 from progature.engine.core.game.handler import GameHandler
 from progature.engine.core.managers import GameManager
-from progature.gui import init_layout, game_window, chapter_window, level_window, quest_window
+from progature.gui import game_window, chapter_window, level_window, quest_window
 
 if __name__ == "__main__":
     game_name = "test_game.json"

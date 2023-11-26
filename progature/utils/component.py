49330428df7f96fe556d57a1e10125bac3deaaaa
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-26 11:32:33 +0330

    bug(utils): fix the ``None`` params bug, add type annotation for ``None`` params

--- progature/utils/component.py
+++ progature/utils/component.py
@@ -16,22 +16,24 @@ def create_bulk_quest(quest_names: List[str]) -> Pot[Quest]:
     return Pot(quest_list)
 
 
-def create_bulk_levels(level_names: List[str], level_quests: List[Pot[Quest]]) -> Pot[Level]:
+def create_bulk_levels(level_names: List[str], level_quests: List[Pot[Quest]] | None = None) -> Pot[Level]:
     level_list = []
     for level_index, level_name in enumerate(level_names):
-        level_list.append(
-            Level(index=level_index, name=level_name, quests=level_quests[level_index])
-        )
+        level = Level(index=level_index, name=level_name)
+        if level_quests:
+            level.quests = level_quests[level_index]
+        level_list.append(level)
 
     return Pot(level_list)
 
 
-def create_bulk_chapters(chapter_names: List[str], chapter_levels: List[Pot[Level]]) -> Pot[Chapter]:
+def create_bulk_chapters(chapter_names: List[str], chapter_levels: List[Pot[Level]] | None = None) -> Pot[Chapter]:
     chapter_list = []
     for chapter_index, chapter_name in enumerate(chapter_names):
-        chapter_list.append(
-            Chapter(index=chapter_index, name=chapter_name, levels=chapter_levels[chapter_index])
-        )
+        chapter = Chapter(index=chapter_index, name=chapter_name)
+        if chapter_levels:
+            chapter.levels = chapter_levels[chapter_index]
+        chapter_list.append(chapter)
 
     return Pot(chapter_list)
 
@@ -40,7 +42,7 @@ def create_skill(name: str) -> Skill:
     return Skill(name=name)
 
 
-def create_game(file_path: str, name: str, skill: Skill, chapters: Pot[Chapter]) -> Game:
+def create_game(file_path: str, name: str, skill: Skill, chapters: Pot[Chapter] | None = None) -> Game:
     game = Game(file_path=file_path,
                 name=name,
                 skill=skill,

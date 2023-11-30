a90e6ef5aa6fe87f9293199b54f3511a8ea3dd1d
parsariyahi <pany.parsariyahi@gmail.com>
2023-11-30 12:52:00 +0330

    feature(utils): add ``docstring`` to create components utils

--- progature/utils/component.py
+++ progature/utils/component.py
@@ -7,6 +7,20 @@ from progature.engine.structures import Pot
 
 
 def create_bulk_quest(quests: List[Dict[Any, Any]]) -> Pot[Quest]:
+    """Create quests in bulk,
+    This function is utility for creating quests,
+    Creation in this function is bulk type based on input (List).
+
+    Parameters
+    ----------
+    quests: list
+        List of quests in dict type.
+
+    Returns
+    -------
+    Pot
+        Pot object that contains Quest's.
+    """
     quest_list = []
     for quest in quests:
         index = quest.get("index", None)
@@ -22,6 +36,20 @@ def create_bulk_quest(quests: List[Dict[Any, Any]]) -> Pot[Quest]:
 
 
 def create_bulk_levels(levels: List[Dict[Any, Any]]) -> Pot[Level]:
+    """Create levels in bulk,
+    This function is utility for creating levels,
+    Creation in this function is bulk type based on input (List).
+
+    Parameters
+    ----------
+    levels: list
+        List of levels in dict type.
+
+    Returns
+    -------
+    Pot
+        Pot object that contains Level's.
+    """
     level_list = []
     for level in levels:
         index = level.get("index", None)
@@ -37,6 +65,20 @@ def create_bulk_levels(levels: List[Dict[Any, Any]]) -> Pot[Level]:
 
 
 def create_bulk_chapters(chapters: List[Dict[Any, Any]]) -> Pot[Chapter]:
+    """Create chapters in bulk,
+    This function is utility for creating chapters,
+    Creation in this function is bulk type based on input (List).
+
+    Parameters
+    ----------
+    chapters: list
+        List of chapters in dict type.
+
+    Returns
+    -------
+    Pot
+        Pot object that contains Chapter's.
+    """
     chapter_list = []
     for chapter in chapters:
         index = chapter.get("index", None)
@@ -52,10 +94,45 @@ def create_bulk_chapters(chapters: List[Dict[Any, Any]]) -> Pot[Chapter]:
 
 
 def create_skill(name: str) -> Skill: 
+    """Create skill,
+    This function is utility for creating skill,
+
+    Parameters
+    ----------
+    name: str
+        Chapter's name that you want to create.
+
+    Returns
+    -------
+    Skill
+        Skill object that is created.
+    """
     return Skill(name=name)
 
 
 def create_game(file_path: str, name: str, skill: Skill, chapters: Pot[Chapter] | None = None) -> Game:
+    """Create Game,
+    This function is utility for creating game,
+
+    Parameters
+    ----------
+    file_path: str
+        The file path that you want to game be saved in.
+
+    name: str
+        Name of the game you want to create
+
+    skill: Skill
+        Skill object that you want to assign to this game.
+
+    chapters: Pot[Chapter], optional
+        Chapter's that the game has.
+
+    Returns
+    -------
+    Game
+        Game object that is created.
+    """
     game = Game(file_path=file_path,
                 name=name,
                 skill=skill,

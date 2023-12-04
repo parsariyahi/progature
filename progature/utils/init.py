161c9666c412389298ac74996bc5bf6032db846a
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-04 11:19:50 +0330

    feature(utils): add ``create_advanced_python_game()``

--- progature/utils/init.py
+++ progature/utils/init.py
@@ -290,7 +290,189 @@ def create_basic_python_game():
 
 
 def create_advanced_python_game():
-    pass
+    game_json = {
+        "file_path": "python_advanced.json",
+        "name": "python advanced",
+        "skill": "python advanced",
+        "is_complete": False,
+        "chapters": [
+            {
+                "index": 0,
+                "name": "advanced function",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "map function",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "how to use map function",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "use map function in your program",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "zip function",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "what is zip function",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "write simple program with zip function",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                "index": 1,
+                "name": "itertools",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "itertools lib",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "use itertools",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "what is __iter__ in classes",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "itertools functions",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "count function",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "cycle function",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                "index": 2,
+                "name": "lambda functions and decorators",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "lambda functions",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "anonymous functions",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "lambda functions VS. regular functions",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "decorators",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "what is wrapper in functions",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "how to call functions in other functions",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                "index": 3,
+                "name": "threading",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "threads",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "what is main thread",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "different types of threads",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "threads VS. processes",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "multi processing VS. threading",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "is threading really simultaneously",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+        ]
+    }
+    skill = create_skill(game_json["skill"])
+    game = create_game(game_json["file_path"],
+                       game_json["name"],
+                       skill,)
+    game.chapters = create_bulk_chapters(chapters=game_json["chapters"])
+    for chapter in game.chapters:
+        chapter.levels = create_bulk_levels(game_json["chapters"][chapter.index]["levels"])
+        for level in chapter.levels:
+            level.quests = create_bulk_quest(game_json["chapters"][chapter.index]["levels"][level.index]["quests"])
+
+    return game
 
 
 def create_data_structure_intro_game():

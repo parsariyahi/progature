558049ace611c0a1385e93d1fc3ed69aa036ef0a
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-05 11:58:38 +0330

    feature(utils): add ``create_data_structure_intro()``

--- progature/utils/init.py
+++ progature/utils/init.py
@@ -476,4 +476,162 @@ def create_advanced_python_game():
 
 
 def create_data_structure_intro_game():
-    pass
+    game_json = {
+        "file_path": "data_structure_intro.json",
+        "name": "data structure intoduction",
+        "skill": "data structure",
+        "is_complete": False,
+        "chapters": [
+            {
+                "index": 0,
+                "name": "introduction to data structure",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "arrays",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "what are arrays",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "how arrays work",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "strings",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "how to use strings",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "strings in low level computing",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                "index": 1,
+                "name": "linear data structures",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "linked lists",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "linked lists VS. arrays",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "how poiners works in linked lists",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "queues",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "queue example in real life",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "do a simple project with queues",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 2,
+                        "name": "stacks",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "stacks VS. queues",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "use stacks in your project",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                "index": 2,
+                "name": "non-linear data structures",
+                "is_complete": False,
+                "levels": [
+                    {
+                        "index": 0,
+                        "name": "trees",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "dfs and bfs",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "use trees",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                    {
+                        "index": 1,
+                        "name": "graphs",
+                        "is_complete": False,
+                        "quests": [
+                            {
+                                "index": 0,
+                                "name": "graph VS. trees",
+                                "is_complete": False,
+                            },
+                            {
+                                "index": 1,
+                                "name": "create social medai with graphs",
+                                "is_complete": False,
+                            },
+                        ]
+                    },
+                ]
+            },
+        ]
+    }
+    skill = create_skill(game_json["skill"])
+    game = create_game(game_json["file_path"],
+                       game_json["name"],
+                       skill,)
+    game.chapters = create_bulk_chapters(chapters=game_json["chapters"])
+    for chapter in game.chapters:
+        chapter.levels = create_bulk_levels(game_json["chapters"][chapter.index]["levels"])
+        for level in chapter.levels:
+            level.quests = create_bulk_quest(game_json["chapters"][chapter.index]["levels"][level.index]["quests"])
+
+    return game

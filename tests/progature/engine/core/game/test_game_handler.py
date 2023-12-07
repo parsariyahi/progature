05aa522055dbdd9c0fdbc3281f6a8f87e3325a80
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-07 11:34:03 +0330

    tests(games): add ``create_test_game``, ``cleanup_test_game`` for testing game ``loder``, ``handler``, ``manager``

--- tests/progature/engine/core/game/test_game_handler.py
+++ tests/progature/engine/core/game/test_game_handler.py
@@ -3,67 +3,89 @@ import json
 
 from progature.engine.core.game.handler import GameHandler
 from progature.engine.core.game.loader import GameLoader
+from tests.utils.games import (
+    create_test_game,
+    cleanup_test_game,
+)
 
 def test_game_handler():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
     handler = GameHandler(py_game_path.absolute())
+    cleanup_test_game()
 
 def test_game_handler_write_on_game():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_game("skill", "basic python3")
+    cleanup_test_game()
 
 def test_game_handler_write_on_chapter():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_chapter("name", "vars", 0)
         handler._write_on_chapter("name", "loooops", 1)
+    cleanup_test_game()
 
 def test_game_handler_write_on_level():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_level("name", "ints", 0, 0)
         handler._write_on_level("name", "for loooops", 1, 1)
+    cleanup_test_game()
 
 def test_game_handler_write_on_quest():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler._write_on_quest("name", "create int obj", 0, 0, 0)
         handler._write_on_quest("is_complete", True, 1, 1, 1)
+    cleanup_test_game()
 
 def test_game_handler_game_complete():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler.game_complete()
+    cleanup_test_game()
 
 def test_game_handler_chapter_complete():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler.chapter_complete(1)
+    cleanup_test_game()
 
 def test_game_handler_level_complete():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler.level_complete(1, 0)
+    cleanup_test_game()
 
 def test_game_handler_quest_complete():
+    create_test_game()
     game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
 
     with GameHandler(py_game_path.absolute()) as handler:
         handler.quest_complete(1, 0, 0)
+    cleanup_test_game()
--- tests/progature/engine/core/game/test_game_manager.py
+++ tests/progature/engine/core/game/test_game_manager.py
@@ -2,9 +2,14 @@ from pathlib import Path
 
 from progature.engine.core.game.manager import GameManager
 from progature.engine.core.game.loader import GameLoader
+from tests.utils.games import (
+    create_test_game,
+    cleanup_test_game,
+)
 
 
 def test_game_manager_init():
+    create_test_game()
     game_name = "test_game.json"
     test_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(test_game_path.absolute())
@@ -12,8 +17,10 @@ def test_game_manager_init():
 
     assert manager.game_name() == game.name
     assert manager.game_skill() == game.skill
+    cleanup_test_game()
 
 def test_game_manager_game_complete():
+    create_test_game()
     game_name = "test_game.json"
     test_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(test_game_path.absolute())
@@ -22,8 +29,10 @@ def test_game_manager_game_complete():
     manager.game_complete()
 
     assert manager.game["is_compelte"] == True
+    cleanup_test_game()
 
 def test_game_manager_game_complete():
+    create_test_game()
     game_name = "test_game.json"
     test_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(test_game_path.absolute())
@@ -31,8 +40,10 @@ def test_game_manager_game_complete():
     manager.game_complete()
 
     assert manager.game.is_complete == True
+    cleanup_test_game()
 
 def test_game_manager_chaper_complete():
+    create_test_game()
     game_name = "test_game.json"
     test_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(test_game_path.absolute())
@@ -42,8 +53,10 @@ def test_game_manager_chaper_complete():
     manager.chapter_complete(chapter_index)
 
     assert manager.game.chapters[chapter_index].is_complete == True
+    cleanup_test_game()
 
 def test_game_manager_level_complete():
+    create_test_game()
     game_name = "test_game.json"
     test_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(test_game_path.absolute())
@@ -54,8 +67,10 @@ def test_game_manager_level_complete():
     manager.level_complete(chapter_index, level_index)
 
     assert manager.game.chapters[chapter_index].levels[level_index].is_complete == True
+    cleanup_test_game()
 
 def test_game_manager_quest_complete():
+    create_test_game()
     game_name = "test_game.json"
     test_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(test_game_path.absolute())
@@ -67,3 +82,4 @@ def test_game_manager_quest_complete():
     manager.quest_complete(chapter_index, level_index, quest_index)
 
     assert manager.game.chapters[chapter_index].levels[level_index].quests[quest_index].is_complete == True
+    cleanup_test_game()

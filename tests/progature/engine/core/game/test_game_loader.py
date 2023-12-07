3b656fd753a8e30bb344ca3ff306c3d2375c4694
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-07 11:34:03 +0330

    tests(games): add ``create_test_game``, ``cleanup_test_game`` for testing game ``loder``, ``handler``, ``manager``

--- tests/progature/engine/core/game/test_game_loader.py
+++ tests/progature/engine/core/game/test_game_loader.py
@@ -3,10 +3,16 @@ import os
 from pathlib import Path
 
 from progature.engine.core.game.loader import GameLoader
+from tests.utils.games import (
+    create_test_game,
+    cleanup_test_game,
+)
 
 def test_game_loader():
-    game_name = "py_game.json"
+    create_test_game()
+    game_name = "test_game.json"
     py_game_path = Path("progature/db/games") / game_name
     game = GameLoader.load(py_game_path.absolute())
 
     print(game, game.is_complete)
+    cleanup_test_game()

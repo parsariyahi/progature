d65515441eba21d52fc782814971bd1c66633dd3
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-10 12:24:15 +0330

    bug(components): fixed import error for ``Dict`` class

--- progature/engine/components/game.py
+++ progature/engine/components/game.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, Union, List
+from typing import Optional, Union, Dict
 
 from progature.engine.structures import Pot
 from progature.engine.components.skill import Skill

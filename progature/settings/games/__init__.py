5c4e20da5f71a2c6b8990e12eedaccad417106f5
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-10 12:17:19 +0330

    feature(settings): moved pre-defined games inside ``settings/games/``

---
+++ progature/settings/games/__init__.py
@@ -0,0 +1,10 @@
+from progature.settings.games.python_basic_game import PYTHON_BASIC_GAME_JSON
+from progature.settings.games.python_advanced_game import PYTHON_ADVANCED_GAME_JSON
+from progature.settings.games.data_structure_into_game import DATA_STRUCTURE_INTRO_GAME_JSON
+
+
+__all__ = [
+    "PYTHON_BASIC_GAME_JSON",
+    "PYTHON_ADVANCED_GAME_JSON",
+    "DATA_STRUCTURE_INTRO_GAME_JSON"
+]
---
+++ progature/settings/games/python_advanced_game.py
@@ -0,0 +1,172 @@
+PYTHON_ADVANCED_GAME_JSON = {
+    "file_path": "python_advanced.json",
+    "name": "python advanced",
+    "skill": "python advanced",
+    "is_complete": False,
+    "chapters": [
+        {
+            "index": 0,
+            "name": "advanced function",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "map function",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "how to use map function",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "use map function in your program",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "zip function",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "what is zip function",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "write simple program with zip function",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 1,
+            "name": "itertools",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "itertools lib",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "use itertools",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "what is __iter__ in classes",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "itertools functions",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "count function",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "cycle function",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 2,
+            "name": "lambda functions and decorators",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "lambda functions",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "anonymous functions",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "lambda functions VS. regular functions",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "decorators",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "what is wrapper in functions",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "how to call functions in other functions",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 3,
+            "name": "threading",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "threads",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "what is main thread",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "different types of threads",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "threads VS. processes",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "multi processing VS. threading",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "is threading really simultaneously",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+    ]
+}

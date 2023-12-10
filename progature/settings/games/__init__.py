19f4d6d8384d971a12990a2bc3017ae661fb9618
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-10 15:18:07 +0330

    feature(settings): add ``test_game`` to our games inventory

--- progature/settings/games/__init__.py
+++ progature/settings/games/__init__.py
@@ -1,10 +1,12 @@
 from progature.settings.games.python_basic_game import PYTHON_BASIC_GAME_JSON
 from progature.settings.games.python_advanced_game import PYTHON_ADVANCED_GAME_JSON
 from progature.settings.games.data_structure_into_game import DATA_STRUCTURE_INTRO_GAME_JSON
+from progature.settings.games.test_game import TEST_GAME_JSON
 
 
 __all__ = [
     "PYTHON_BASIC_GAME_JSON",
     "PYTHON_ADVANCED_GAME_JSON",
-    "DATA_STRUCTURE_INTRO_GAME_JSON"
+    "DATA_STRUCTURE_INTRO_GAME_JSON",
+    "TEST_GAME_JSON"
 ]
---
+++ progature/settings/games/test_game.py
@@ -0,0 +1,267 @@
+TEST_GAME_JSON = {
+    "file_name": "test_game.json",
+    "name": "python basic",
+    "skill": "python basic",
+    "is_complete": False,
+    "chapters": [
+        {
+            "index": 0,
+            "name": "variables",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "integers",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "create integers",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "print some integers",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "strings",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "create strings",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "print strings",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 2,
+                            "name": "write a program that prints your name",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 2,
+                    "name": "list and tupes",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "create list and tuples",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "lists VS. tuples",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 2,
+                            "name": "store names in a list and tuples and work with them",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 1,
+            "name": "operations",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "addition",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "add two integers",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "concat two strings",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "subtraction",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "subtrac integers",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "use subtraction on strings",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 2,
+                            "name": "write basic calculator",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 2,
+                    "name": "multiplication and division",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "multiply on integers",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "division in python",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 2,
+                            "name": "use math built-in library",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 2,
+            "name": "loops",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "for loops",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "loop on lists",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "loop on tuples",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "while loop",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "create command line interface",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "use while loop like other languages loops",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 3,
+            "name": "functions",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "args and params",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "args VS. params",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "type hintings",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "returns and yields",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "returns VS. yields",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "when to use which",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            "index": 4,
+            "name": "classes",
+            "is_complete": False,
+            "levels": [
+                {
+                    "index": 0,
+                    "name": "OOP",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "basic concepts of OOP",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "dunder methods like __init__",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+                {
+                    "index": 1,
+                    "name": "inheritance",
+                    "is_complete": False,
+                    "quests": [
+                        {
+                            "index": 0,
+                            "name": "multi inheritance",
+                            "is_complete": False,
+                        },
+                        {
+                            "index": 1,
+                            "name": "super() function in classes",
+                            "is_complete": False,
+                        },
+                    ]
+                },
+            ]
+        },
+    ]
+}

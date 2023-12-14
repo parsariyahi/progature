08679638e00b9b4be7f9b3b5e15dbf8853fcb4df
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-14 10:58:47 +0330

    tests(utils): add ``len()`` test for ``load_all_games()`` test

--- tests/progature/utils/test_loader_utils.py
+++ tests/progature/utils/test_loader_utils.py
@@ -3,5 +3,6 @@ from progature.utils.loader import load_all_games
 def test_load_all_games(game):
     games_pot = load_all_games()
 
+    assert len(games_pot) == 1
     for g in games_pot:
         assert g == game

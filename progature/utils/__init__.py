52201dacd2e410f380203e67c802d0795e390777
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-31 12:35:34 +0330

    feature(utils): add all utils inside ``utils/__init__

---
+++ progature/utils/__init__.py
@@ -0,0 +1,29 @@
+from progature.utils.games import (
+    create_data_structure_intro_game,
+    create_advanced_python_game,
+    create_basic_python_game,
+    create_all_games,
+)
+from progature.utils.component import (
+    create_bulk_chapters,
+    create_bulk_levels,
+    create_bulk_quest,
+    create_game,
+)
+from progature.utils.loader import (
+    load_all_games,
+)
+
+__all__ = [
+    "create_data_structure_intro_game",
+    "create_advanced_python_game",
+    "create_basic_python_game",
+    "create_all_games",
+
+    "create_bulk_chapters",
+    "create_bulk_levels",
+    "create_bulk_quest",
+    "create_game",
+
+    "load_all_games",
+]

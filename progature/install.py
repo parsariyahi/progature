89b2c69e4b6845f2e6ab1edeb6ab13d2a99a0fa4
parsariyahi <pany.parsariyahi@gmail.com>
2023-12-31 12:37:40 +0330

    feature(install): update ``utils`` module import

--- progature/install.py
+++ progature/install.py
@@ -1,10 +1,9 @@
 import os
 import sys
-from pathlib import Path
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from progature.utils.init import create_all_games
+from progature.utils import create_all_games
 
 
 if __name__ == "__main__":

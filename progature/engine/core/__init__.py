03c4c29bfd0b53db4b17aa95342c2f2ff0335dd1
parsariyahi <pany.parsariyahi@gmail.com>
2024-01-04 14:47:28 +0330

    feature(core): make ``core`` a module and add ``__init__.py``

---
+++ progature/engine/core/__init__.py
@@ -0,0 +1,14 @@
+"""`core` module is main part of the app.
+This module is responsible for working with `Game` objects.
+
+`core` module is an interface for app to work with `Game` objects.
+writing and changing and managing `Game` objects are implemented here.
+
+
+Notes
+-----
+we have `game` submodule that contains<br>
+1. handler<br>
+2. manager<br>
+3. loader<br>
+"""

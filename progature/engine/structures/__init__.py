2b02fdacf48f049f90112670b40ad638de44c306
parsariyahi <pany.parsariyahi@gmail.com>
2024-01-06 11:03:50 +0330

    docs(structures): add ``docstring`` for ``structures`` module

--- progature/engine/structures/__init__.py
+++ progature/engine/structures/__init__.py
@@ -1,3 +1,14 @@
+"""`strcuture` module is custom data structures that we use in `progature`
+
+Notes
+-----
+we have these `structures`: <br>
+1 - Pot
+"""
 from progature.engine.structures.node import Node
 from progature.engine.structures.linkedlist import DLinkedList
 from progature.engine.structures.pot import Pot
+
+__all__ = [
+    "Pot"
+]
